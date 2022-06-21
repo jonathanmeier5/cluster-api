a9ce0e6c33e81e0fd82e277a022aa1f316e0d71c
Victor Pineda <vgonzla@amazon.com>
2022-06-21 07:50:19 -0700

    Add feature to specifiy additional host containers in BR
    Host containers are a feature within BR that allows us to pull images
    without the need of having to bootstrap kuberentes. Such containers can
    be superpowered and user-data can be attached to each one of them. As
    such, this commit creates the `BottlerocketHostContainer` struct to allow
    the user to customize the those fields. Users can specify an arbitrary
    number of host containers in the `AdditionalHostContainers` field.
    
    This commit also does some refactoring around the templating system BR
    has to generate the TOML files. It generifies the host-container
    template to be reused as much as a user wants to.
    
    SIM: https://i.amazon.com/P66557529
    cr: https://code.amazon.com/reviews/CR-71408825

--- bootstrap/kubeadm/internal/controllers/kubeadmconfig_controller.go
+++ bootstrap/kubeadm/internal/controllers/kubeadmconfig_controller.go
@@ -465,9 +465,10 @@ func (r *KubeadmConfigReconciler) handleClusterNotInitialized(ctx context.Contex
 		}
 
 		bottlerocketConfig = &bottlerocket.BottlerocketConfig{
-			Pause:                 scope.Config.Spec.ClusterConfiguration.Pause,
-			BottlerocketBootstrap: scope.Config.Spec.ClusterConfiguration.BottlerocketBootstrap,
-			BottlerocketControl:   scope.Config.Spec.ClusterConfiguration.BottlerocketControl,
+			Pause:                            scope.Config.Spec.ClusterConfiguration.Pause,
+			BottlerocketBootstrap:            scope.Config.Spec.ClusterConfiguration.BottlerocketBootstrap,
+			BottlerocketControl:              scope.Config.Spec.ClusterConfiguration.BottlerocketControl,
+			BottlerocketCustomHostContainers: scope.Config.Spec.ClusterConfiguration.BottlerocketHostContainers,
 		}
 		if scope.Config.Spec.ClusterConfiguration.Proxy.HTTPSProxy != "" {
 			bottlerocketConfig.ProxyConfiguration = scope.Config.Spec.ClusterConfiguration.Proxy
@@ -667,9 +668,10 @@ func (r *KubeadmConfigReconciler) joinWorker(ctx context.Context, scope *Scope) 
 		})
 	case bootstrapv1.Bottlerocket:
 		bottlerocketConfig := &bottlerocket.BottlerocketConfig{
-			Pause:                 scope.Config.Spec.JoinConfiguration.Pause,
-			BottlerocketBootstrap: scope.Config.Spec.JoinConfiguration.BottlerocketBootstrap,
-			BottlerocketControl:   scope.Config.Spec.JoinConfiguration.BottlerocketControl,
+			Pause:                            scope.Config.Spec.JoinConfiguration.Pause,
+			BottlerocketBootstrap:            scope.Config.Spec.JoinConfiguration.BottlerocketBootstrap,
+			BottlerocketControl:              scope.Config.Spec.JoinConfiguration.BottlerocketControl,
+			BottlerocketCustomHostContainers: scope.Config.Spec.JoinConfiguration.BottlerocketCustomHostContainers,
 		}
 		if scope.Config.Spec.JoinConfiguration.Proxy.HTTPSProxy != "" {
 			bottlerocketConfig.ProxyConfiguration = scope.Config.Spec.JoinConfiguration.Proxy
@@ -793,9 +795,10 @@ func (r *KubeadmConfigReconciler) joinControlplane(ctx context.Context, scope *S
 		})
 	case bootstrapv1.Bottlerocket:
 		bottlerocketConfig := &bottlerocket.BottlerocketConfig{
-			Pause:                 scope.Config.Spec.JoinConfiguration.Pause,
-			BottlerocketBootstrap: scope.Config.Spec.JoinConfiguration.BottlerocketBootstrap,
-			BottlerocketControl:   scope.Config.Spec.JoinConfiguration.BottlerocketControl,
+			Pause:                            scope.Config.Spec.JoinConfiguration.Pause,
+			BottlerocketBootstrap:            scope.Config.Spec.JoinConfiguration.BottlerocketBootstrap,
+			BottlerocketControl:              scope.Config.Spec.JoinConfiguration.BottlerocketControl,
+			BottlerocketCustomHostContainers: scope.Config.Spec.JoinConfiguration.BottlerocketCustomHostContainers,
 		}
 		if scope.Config.Spec.JoinConfiguration.Proxy.HTTPSProxy != "" {
 			bottlerocketConfig.ProxyConfiguration = scope.Config.Spec.JoinConfiguration.Proxy
