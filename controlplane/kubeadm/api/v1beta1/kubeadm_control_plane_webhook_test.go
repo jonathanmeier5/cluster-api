2cfba5d0698bdb35f9ecbd24b62ed38508cda31d
Cavaughn Browne <cxbrowne@amazon.com>
2023-07-20 11:05:49 -0500

    allow registry mirror configurations to be mutable for BR

--- controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook_test.go
+++ controlplane/kubeadm/api/v1beta1/kubeadm_control_plane_webhook_test.go
@@ -302,6 +302,10 @@ func TestKubeadmControlPlaneValidateUpdate(t *testing.T) {
 							ImageTag:        "1.6.5",
 						},
 					},
+					RegistryMirror: bootstrapv1.RegistryMirrorConfiguration{
+						Endpoint: "https://1.1.1.1:1111",
+						CACert:   "test-cert",
+					},
 				},
 				JoinConfiguration: &bootstrapv1.JoinConfiguration{
 					Discovery: bootstrapv1.Discovery{
@@ -311,6 +315,10 @@ func TestKubeadmControlPlaneValidateUpdate(t *testing.T) {
 					},
 					NodeRegistration: bootstrapv1.NodeRegistrationOptions{
 						Name: "test",
+					},
+					RegistryMirror: bootstrapv1.RegistryMirrorConfiguration{
+						Endpoint: "https://1.1.1.1:1111",
+						CACert:   "test-cert",
 					},
 				},
 				PreKubeadmCommands: []string{
@@ -698,6 +706,18 @@ func TestKubeadmControlPlaneValidateUpdate(t *testing.T) {
 	switchFromCloudInitToIgnition.Spec.KubeadmConfigSpec.Mounts = []bootstrapv1.MountPoints{
 		{"/var/lib/testdir", "/var/lib/etcd/data"},
 	}
+
+	validUpdateClusterConfigRegistryMirrorCACert := before.DeepCopy()
+	validUpdateClusterConfigRegistryMirrorCACert.Spec.KubeadmConfigSpec.ClusterConfiguration.RegistryMirror.CACert = "foo:bar"
+
+	validUpdateJoinConfigRegistryMirrorCACert := before.DeepCopy()
+	validUpdateJoinConfigRegistryMirrorCACert.Spec.KubeadmConfigSpec.JoinConfiguration.RegistryMirror.CACert = "foo:bar"
+
+	validUpdateClusterConfigRegistryMirrorEndpoint := before.DeepCopy()
+	validUpdateClusterConfigRegistryMirrorEndpoint.Spec.KubeadmConfigSpec.ClusterConfiguration.RegistryMirror.Endpoint = "https://0.0.0.0:6443"
+
+	validUpdateJoinConfigRegistryMirrorEndpoint := before.DeepCopy()
+	validUpdateJoinConfigRegistryMirrorEndpoint.Spec.KubeadmConfigSpec.JoinConfiguration.RegistryMirror.Endpoint = "https://0.0.0.0:6443"
 
 	tests := []struct {
 		name                  string
@@ -1103,6 +1123,31 @@ func TestKubeadmControlPlaneValidateUpdate(t *testing.T) {
 			expectErr: false,
 			before:    before,
 			kcp:       validUpdateJoinConfBRCustomBootstrapContainers,
+		},
+		{
+			name:      "should allow changes to join configuration registry mirror caCert",
+			expectErr: false,
+			before:    before,
+			kcp:       validUpdateJoinConfigRegistryMirrorCACert,
+		},
+		{
+			name:      "should allow changes to join configuration registry mirror endpoint",
+			expectErr: false,
+			before:    before,
+			kcp:       validUpdateJoinConfigRegistryMirrorEndpoint,
+		},
+		{
+			name:      "should allow changes to cluster configuration registry mirror caCert",
+			expectErr: false,
+			before:    before,
+			kcp:       validUpdateClusterConfigRegistryMirrorCACert,
+		},
+
+		{
+			name:      "should allow changes to cluster configuration registry mirror endpoint",
+			expectErr: false,
+			before:    before,
+			kcp:       validUpdateClusterConfigRegistryMirrorEndpoint,
 		},
 	}
 
